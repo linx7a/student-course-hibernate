b0419e88846d613bd9d63e78b3a36067cbc059be
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-08-19 21:44:15 +0300

    feat: add TransactionHelper for reusable transaction handling

---
+++ src/main/java/linx7a/TransactionHelper.java
@@ -0,0 +1,46 @@
+package linx7a;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Component
+public class TransactionHelper {
+    private final SessionFactory sessionFactory;
+
+    public TransactionHelper(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+
+    }
+    public void executeInTransaction(Consumer<Session> action) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            action.accept(session);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+    public<T> T executeInTransaction(Function<Session, T> action) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            var result = action.apply(session);
+            transaction.commit();
+            return result;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+}
